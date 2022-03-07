888f5ea209fa189e6cb26833b4dec24ea4ec07cc
magadan <cristian@magadan.com.br>
2022-03-07 02:38:51 -0300

    .
    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import A from "./A";
 import { Heading, Box } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { Image } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
 
 /**
  * Header for any page
@@ -12,12 +13,12 @@ function Header(props: React.ComponentPropsWithRef<"header">) {
     <header {...props}>
       <Box pt="1rem" pb="1rem" pl="20vw" pr="20vw" bg="gray.100">
         <Heading size="xl">
-		  <Image src="https://magadan.github.io/virtualpinballpackages/logo.png" alt="Visual Pinball" object-position="center"/>
+		  <center><Image src="https://magadan.github.io/virtualpinballpackages/logo.png" alt="Visual Pinball"/></center>
           <Link to="/"></Link> 
         </Heading>
         <Heading size="sm">
           Find <A href="https://github.com/magadan/mgd-packages/tree/master/bucket">Virtual Pinball Resources</A> instantly from the comfort of your browser, like tables, wheels images and etc  and install with scoop.
-		  <Image src="http://i.imgur.com/QkcAbDy.png" alt="Visual Pinball" object-position="center"/>
+		  <center><Image src="http://i.imgur.com/QkcAbDy.png" alt="Visual Pinball"/></center>
         </Heading>
         {props.children}
       </Box>
